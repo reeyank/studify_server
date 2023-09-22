5b5be604066f7f15a2ca56868c8afd770917d2bd
Reeyan Khimani <reeyankhimani@Reeyans-MacBook-Air.local>
2023-09-22 11:04:11 -0400

    update

--- routes/record.mjs
+++ routes/record.mjs
@@ -6,7 +6,7 @@ const router = express.Router();
 
 // This section will help you get a list of all the records.
 router.get("/", async (req, res) => {
-  let collection = await db.collection("records");
+  let collection = await db.collection("userInfo");
   let results = await collection.find({}).toArray();
   res.send(results).status(200);
 });
