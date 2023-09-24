dfd28cd1035ea60c3afb878140321f31ee1edb72
Reeyan Khimani <reeyankhimani@Reeyans-Air.attlocal.net>
2023-09-23 20:52:49 -0400

    fix reward api

--- routes/reward.mjs
+++ routes/reward.mjs
@@ -6,7 +6,7 @@ const rewardInfo = express.Router();
 
 // This section will help you get a list of all the records.
 rewardInfo.get("/", async (req, res) => {
-  let collection = await db.collection("userInfo");
+  let collection = await db.collection("rewardInfo");
   let results = await collection.find({}).toArray();
   res.send(results).status(200);
 });
